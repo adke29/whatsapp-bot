20f082d081ff5504dc5f1b29b183bc2246bb73a8
adke29 <adrian.kevin@yahoo.com>
2023-12-15 14:05:20 +0800

    add message listener

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: ['--no-sandbox'],
+    }
+});
+
+client.initialize();
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.initialize();
+client.on('loading_screen', (percent, message) => {
+    console.log('LOADING SCREEN', percent,'% ', message);
+});
+
+client.on('authenticated', () => {
+    console.log('AUTHENTICATED');
+});
+
+client.on('message', message => {
+    console.log(message.body);
+    message.sendMessage('apaan');
+});
+
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
+});
